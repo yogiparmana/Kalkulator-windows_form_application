ac466000fc57209238eb7552a87faf534fc658ec
yogiparmana <yogiparmanaiwayan1@gmail.com>
2021-03-03 05:05:37 +0800

    Add project files.

---
+++ Kalkulator/Form1.Designer.cs
@@ -0,0 +1,347 @@
+﻿
+namespace Kalkulator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxkal = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button0 = new System.Windows.Forms.Button();
+            this.buttonkurang = new System.Windows.Forms.Button();
+            this.buttonkali = new System.Windows.Forms.Button();
+            this.buttonbagi = new System.Windows.Forms.Button();
+            this.buttontambah = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonsama = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxkal
+            // 
+            this.textBoxkal.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.textBoxkal.Font = new System.Drawing.Font("Showcard Gothic", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxkal.ForeColor = System.Drawing.SystemColors.MenuBar;
+            this.textBoxkal.Location = new System.Drawing.Point(8, 41);
+            this.textBoxkal.Name = "textBoxkal";
+            this.textBoxkal.ReadOnly = true;
+            this.textBoxkal.Size = new System.Drawing.Size(254, 32);
+            this.textBoxkal.TabIndex = 0;
+            this.textBoxkal.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.textBoxkal.TextChanged += new System.EventHandler(this.textBoxkal_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Font = new System.Drawing.Font("Showcard Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(8, 231);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(63, 54);
+            this.button1.TabIndex = 1;
+            this.button1.Tag = "angka";
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Font = new System.Drawing.Font("Showcard Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button2.Location = new System.Drawing.Point(69, 231);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(63, 54);
+            this.button2.TabIndex = 2;
+            this.button2.Tag = "angka";
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button3.Font = new System.Drawing.Font("Showcard Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button3.Location = new System.Drawing.Point(130, 231);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(63, 54);
+            this.button3.TabIndex = 3;
+            this.button3.Tag = "angka";
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button6.Font = new System.Drawing.Font("Showcard Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button6.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button6.Location = new System.Drawing.Point(130, 176);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(63, 54);
+            this.button6.TabIndex = 6;
+            this.button6.Tag = "angka";
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button5.Font = new System.Drawing.Font("Showcard Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button5.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button5.Location = new System.Drawing.Point(69, 176);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(63, 54);
+            this.button5.TabIndex = 5;
+            this.button5.Tag = "angka";
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button4.Font = new System.Drawing.Font("Showcard Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button4.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button4.Location = new System.Drawing.Point(8, 176);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(63, 54);
+            this.button4.TabIndex = 4;
+            this.button4.Tag = "angka";
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button9.Font = new System.Drawing.Font("Showcard Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button9.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button9.Location = new System.Drawing.Point(130, 121);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(63, 54);
+            this.button9.TabIndex = 9;
+            this.button9.Tag = "angka";
+            this.button9.Text = "9";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button8.Font = new System.Drawing.Font("Showcard Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button8.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button8.Location = new System.Drawing.Point(69, 121);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(63, 54);
+            this.button8.TabIndex = 8;
+            this.button8.Tag = "angka";
+            this.button8.Text = "8";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button7.Font = new System.Drawing.Font("Showcard Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button7.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button7.Location = new System.Drawing.Point(8, 121);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(63, 54);
+            this.button7.TabIndex = 7;
+            this.button7.Tag = "angka";
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button0
+            // 
+            this.button0.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button0.Font = new System.Drawing.Font("Showcard Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button0.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button0.Location = new System.Drawing.Point(8, 286);
+            this.button0.Name = "button0";
+            this.button0.Size = new System.Drawing.Size(185, 54);
+            this.button0.TabIndex = 10;
+            this.button0.Text = "0";
+            this.button0.UseVisualStyleBackColor = false;
+            this.button0.Click += new System.EventHandler(this.button0_Click);
+            // 
+            // buttonkurang
+            // 
+            this.buttonkurang.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.buttonkurang.Font = new System.Drawing.Font("Showcard Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.buttonkurang.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.buttonkurang.Location = new System.Drawing.Point(199, 176);
+            this.buttonkurang.Name = "buttonkurang";
+            this.buttonkurang.Size = new System.Drawing.Size(63, 54);
+            this.buttonkurang.TabIndex = 13;
+            this.buttonkurang.Text = "-";
+            this.buttonkurang.UseVisualStyleBackColor = false;
+            this.buttonkurang.Click += new System.EventHandler(this.buttonkurang_Click);
+            // 
+            // buttonkali
+            // 
+            this.buttonkali.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.buttonkali.Font = new System.Drawing.Font("Showcard Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.buttonkali.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.buttonkali.Location = new System.Drawing.Point(199, 231);
+            this.buttonkali.Name = "buttonkali";
+            this.buttonkali.Size = new System.Drawing.Size(63, 54);
+            this.buttonkali.TabIndex = 12;
+            this.buttonkali.Text = "X";
+            this.buttonkali.UseVisualStyleBackColor = false;
+            this.buttonkali.Click += new System.EventHandler(this.buttonkali_Click);
+            // 
+            // buttonbagi
+            // 
+            this.buttonbagi.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.buttonbagi.Font = new System.Drawing.Font("Showcard Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.buttonbagi.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.buttonbagi.Location = new System.Drawing.Point(199, 286);
+            this.buttonbagi.Name = "buttonbagi";
+            this.buttonbagi.Size = new System.Drawing.Size(63, 54);
+            this.buttonbagi.TabIndex = 11;
+            this.buttonbagi.Text = ":";
+            this.buttonbagi.UseVisualStyleBackColor = false;
+            this.buttonbagi.Click += new System.EventHandler(this.buttonbagi_Click);
+            // 
+            // buttontambah
+            // 
+            this.buttontambah.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.buttontambah.Font = new System.Drawing.Font("Showcard Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.buttontambah.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.buttontambah.Location = new System.Drawing.Point(199, 121);
+            this.buttontambah.Name = "buttontambah";
+            this.buttontambah.Size = new System.Drawing.Size(63, 54);
+            this.buttontambah.TabIndex = 14;
+            this.buttontambah.Text = "+";
+            this.buttontambah.UseVisualStyleBackColor = false;
+            this.buttontambah.Click += new System.EventHandler(this.buttontambah_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Showcard Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(35, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(209, 20);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Kalkulator Sederhana";
+            // 
+            // buttonsama
+            // 
+            this.buttonsama.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.buttonsama.Font = new System.Drawing.Font("Showcard Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.buttonsama.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.buttonsama.Location = new System.Drawing.Point(199, 79);
+            this.buttonsama.Name = "buttonsama";
+            this.buttonsama.Size = new System.Drawing.Size(64, 36);
+            this.buttonsama.TabIndex = 16;
+            this.buttonsama.Text = "=";
+            this.buttonsama.UseVisualStyleBackColor = false;
+            this.buttonsama.Click += new System.EventHandler(this.buttonsama_Click);
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button10.Font = new System.Drawing.Font("Showcard Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button10.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button10.Location = new System.Drawing.Point(12, 79);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(64, 36);
+            this.button10.TabIndex = 17;
+            this.button10.Text = "C";
+            this.button10.UseVisualStyleBackColor = false;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(269, 353);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.buttonsama);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttontambah);
+            this.Controls.Add(this.buttonkurang);
+            this.Controls.Add(this.buttonkali);
+            this.Controls.Add(this.buttonbagi);
+            this.Controls.Add(this.button0);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxkal);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "Form1";
+            this.Text = "App";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxkal;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button0;
+        private System.Windows.Forms.Button buttonkurang;
+        private System.Windows.Forms.Button buttonkali;
+        private System.Windows.Forms.Button buttonbagi;
+        private System.Windows.Forms.Button buttontambah;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonsama;
+        private System.Windows.Forms.Button button10;
+    }
+}
+
---
+++ Kalkulator/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kalkulator
+{
+    public partial class Form1 : Form
+    {
+        char simbol;
+        float angka1,angka2;
+        float hasil;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button0_Click(object sender, EventArgs e)
+        {
+            if(textBoxkal.Text != string.Empty)
+            {
+                textBoxkal.Text += "0";
+            }
+        }
+
+        private void textBoxkal_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBoxkal.Text += "1";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBoxkal.Text += "2";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBoxkal.Text += "3";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBoxkal.Text += "4";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBoxkal.Text += "5";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBoxkal.Text += "6";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBoxkal.Text += "7";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textBoxkal.Text += "8";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBoxkal.Text += "9";
+        }
+
+        private void buttonbagi_Click(object sender, EventArgs e)
+        {
+            angka1 = int.Parse(this.textBoxkal.Text);
+            simbol = ':';
+            textBoxkal.Text += " : ";
+        }
+
+        private void buttonkali_Click(object sender, EventArgs e)
+        {
+            angka1 = float.Parse(this.textBoxkal.Text);
+            simbol = 'x';
+            textBoxkal.Text += " x ";
+        }
+
+        private void buttonkurang_Click(object sender, EventArgs e)
+        {
+            angka1 = float.Parse(this.textBoxkal.Text);
+            simbol = '-';
+            textBoxkal.Text += " - ";
+        }
+
+        private void buttontambah_Click(object sender, EventArgs e)
+        {
+            angka1 = int.Parse(this.textBoxkal.Text);
+            simbol = '+';
+            textBoxkal.Text += " + ";
+        }
+
+        private void buttonsama_Click(object sender, EventArgs e)
+        {
+            
+            if (this.textBoxkal.Text != string.Empty || simbol == null)
+            {
+
+                string[] arr = this.textBoxkal.Text.Split(' ');
+
+                if (arr.Length == 3 && arr[2] != "") 
+                {
+                    angka2 = int.Parse(arr[2]);
+                    switch (simbol)
+                    {
+                        case '+':
+                            hasil = angka1 + angka2;
+                            break;
+                        case '-':
+                            hasil = angka1 - angka2;
+                            break;
+                        case ':':
+                            hasil = angka1 / angka2;
+                            break;
+                        case 'x':
+                            hasil = angka1 * angka2;
+                            break;
+                        default:
+                            break;
+                    }
+                    textBoxkal.Text = hasil.ToString();
+                }
+                
+            }
+            
+
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBoxkal.Text = string.Empty;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            textBoxkal.Clear();
+        }
+    }
+}
